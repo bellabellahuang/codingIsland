92ead3bd98e9ba2ddf411f701a4741a808a6a36a
bellabellahuang <bellahuang1012@gmail.com>
2019-04-01 16:43:54 -0700

    added codes for string formatting

--- scripts/string-examples.py
+++ scripts/string-examples.py
@@ -7,6 +7,17 @@ print(example.split('/'))
 print('/'.join(example.split('/')[2:]))
 # output: c/d
 
+
 # -----------------------------
 # replacing the last character of a string
 print(example[:-1] + 'e')
+
+
+# -----------------------------
+# formatting string with variable placer
+example = """
+    SELECT * FROM {table}
+    WHERE id = '{id}';
+"""
+
+print(example.format(table="students", id="123"))
