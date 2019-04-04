2de7d389182551f9807747dca1a55e55716f976e
bellabellahuang <bellahuang1012@gmail.com>
2019-04-04 13:10:21 -0700

    added conditional expression script

--- README.md
+++ README.md
@@ -5,10 +5,11 @@
 * [Extra dependencies & libraries](libraries.md)
 * [GIT](git.md)
 * [SSH](ssh.md)
-
+* [AWS](aws.md)
 
 ### Scripts
 
 * [datetime](datetime.md)
 * [json](./scripts/json-examples.py)
 * [string](./scripts/string-examples.py)
+* [Conditional Expression](./scripts/conditional-expression.py)
---
+++ scripts/conditional-expression.py
@@ -0,0 +1,29 @@
+# regular if-else statement
+if condition:
+    # do something when condition is true
+    statement_1
+    statement_2
+    ...
+    statement_n
+else:
+    # do something when condition is false
+    statement_1
+    statement_2
+    ...
+    statement_n
+
+
+# one line statement
+m = a if condition else b
+
+if condition: return something
+
+
+# Guard Clause
+if condition_A:
+    return result_A
+if condition_B:
+    return result_B
+if condition_C:
+    return result_C
+return default_result
