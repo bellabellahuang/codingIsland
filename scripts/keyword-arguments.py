624c3f32dbc7c5885ab49ff04a0c0c9bad40a0de
bellabellahuang <bellahuang1012@gmail.com>
2019-04-11 13:51:01 -0700

    added script for argv

--- scripts/keyword-arguments.py
+++ scripts/keyword-arguments.py
@@ -12,3 +12,16 @@ myFun(**{"a": "1", "b": "2", "c": "3"})
 
 ** is required to unpack the dictionay
 """
+
+
+def sum(*argv):
+    sum = 0
+    for num in argv:
+        sum += num
+    print("sum: " + str(sum))
+
+
+"""
+create a function with variable number of arguments
+sum(1, 2, 3)
+"""
